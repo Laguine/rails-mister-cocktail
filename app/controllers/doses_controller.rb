c0d1a7cc9eb47b06a37a32136dda14f30e516570
Alexis Guinebretiere <alexis.guineb@gmail.com>
2017-05-18 17:44:15 +0200

    last commit of the day

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+      raise
+    end
+  end
+
+
+  def destroy
+  end
+
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+end
